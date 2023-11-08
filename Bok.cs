4cc5f3d9a406b81f5ef97f14f34cbcdd28f3c603
roxzlir <emil.ch.nordin@gmail.com>
2023-11-08 14:35:50 +0100

    Add project files.

---
+++ Bok.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenguinLibrary
+{
+    public class Bok
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ISBN { get; set; }
+
+        public Bok(string title, string author, int iSBN)
+        {
+            Title = title;
+            Author = author;
+            ISBN = iSBN;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}\nAuthor: {Author}\nISBN: {ISBN}");
+        }
+
+    }
+}
