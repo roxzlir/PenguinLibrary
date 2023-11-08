b43247429c5f0a022f679afd265ffb69fd7ab7f3
Angelica Lindström <nutrition.angelica@gmail.com>
2023-11-08 14:54:49 +0100

    Media Classen // Angelica

---
+++ Media.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenguinLibrary
+{
+    public abstract class Media
+    {
+        public abstract string PlayBook();
+
+        public abstract string ShowInfo();
+
+    }
+}
